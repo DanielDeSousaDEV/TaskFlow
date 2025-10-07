94f05570c0a02a95e92e4ec18edb03d9536b13c3
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-07 18:31:12 -0300

    chore: adding morgan to better precision on debug

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,19 @@ import UserRoutes from "./routes/UserRoutes";
 import { ErrorHandler } from "./middlewares/ErrorHandler";
 import TaskRoutes from "./routes/TaskRoutes";
 import { env } from "./env";
+import { NotFound } from "./middlewares/NotFound";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(express.json())
 
 app.use(UserRoutes)
 app.use(TaskRoutes)
 
+// app.use(NotFound)
 app.use(ErrorHandler)
 
 app.listen(env.PORT, env.HOST, () => {
